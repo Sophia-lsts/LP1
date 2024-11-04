30b786a53d0e4827b3666dc8dea942a15443bda3
Sophia <sophialimasantos4@gmail.com>
2024-11-04 16:59:44 -0300

    nao sei oq to fznd

--- campoMinado/jogo.c
+++ campoMinado/jogo.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+
+#define lin 5
 
 void timer(/*parametro encontrou bomba ou ganhou*/){
 
@@ -11,14 +14,81 @@ void timer(/*parametro encontrou bomba ou ganhou*/){
 
         sleep(1);
 
+
+
+
         segundos++;
     };
 
 }
 
+typedef struct {
+    int branco;
+} nada;
+
+nada M[lin][lin];
+
+/*void preencheCampo(){
+    int i;
+    int j;
+
+
+    srand(time(NULL));
+
+
+
+}*/
+
+void testeRecursao(int i, int j){
+
+    if(M[i][j].branco == 0){
+        M[i][j].branco = 0;
+
+        testeRecursao(i-1,j+1);
+        testeRecursao(i-1,j-1);
+        testeRecursao(i-1, j);
+        testeRecursao(i, j-1);
+        testeRecursao(i, j+1);
+        testeRecursao(i+1, j+1);
+        testeRecursao(i+1, j-1);
+        testeRecursao(i+1, j);
+    }
+
+}
+
 int main(){
 
-    timer();
+    //timer();
+    int i;
+    int j;
+    int linha, coluna;
+
+    for(i=0;i<lin;i++){
+        for(j=0;j<lin;j++){
+            scanf("%d", &M[i][j].branco);
+        }
+    }
+
+    for(i=0;i<lin;i++){
+        printf("\n");
+        for(j=0;j<lin;j++){
+            printf(" %d ", M[i][j].branco);
+        }
+    }
+
+    printf("/nlin");
+    scanf("%d", &linha);
+    printf("/ncol");
+    scanf("%d", &coluna);
+
+    testeRecursao(linha, coluna);
+
+    for(i=0;i<lin;i++){
+        printf("\n");
+        for(j=0;j<lin;j++){
+            printf(" %d ", M[i][j].branco);
+        }
+    }
 
     return 0;
 
